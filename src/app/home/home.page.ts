d13b9ada0afc78ad03d01839f5f89ebbfcf76c73
camilamouraw <camilaaoliver10@gmail.com>
2022-03-25 22:45:07 -0300

    Aplicativo criado e primeira conta executando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  
+  quantidadeCigarros = 0;
+  quantidadeAnos = 0;
+  totalCigarros = 0;  
+
+  tempoPerdido = 0;
+
+  minutosPerdidos = 0;
+  horasPerdidas = 0;
+  diasPerdidos = 0;
+  mesesPerdidos = 0;
+  anosPerdidos = 0;
+
 
   constructor() {}
 
+  CalcularTempo(){
+
+    this.minutosPerdidos = this.quantidadeCigarros * 360 * this.quantidadeAnos * 10;
+    this.horasPerdidas = this.minutosPerdidos / 60;
+    this.diasPerdidos = this.horasPerdidas / 24;
+    this.mesesPerdidos = this.diasPerdidos / 30;
+    this.anosPerdidos = this.mesesPerdidos / 12;
+
+  }
+   
 }
